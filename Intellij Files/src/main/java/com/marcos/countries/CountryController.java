913df6259f4e76a653e87a615ddfffe616993d4c
Marcos Lira <liram1995@gmail.com>
2019-10-07 16:32:40 -0500

    Changed port to 2018 && getAllCountries working

--- Intellij Files/src/main/java/com/marcos/countries/CountryController.java
+++ Intellij Files/src/main/java/com/marcos/countries/CountryController.java
@@ -1,12 +1,21 @@
 package com.marcos.countries;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/country")
+@RequestMapping("/names")
 public class CountryController
 {
-
+    // localhost:2019/names/all
+    @GetMapping(value = "/all", produces = {"application/json"})
+    public ResponseEntity<?> getAllCountries()
+    {
+        CountriesApplication.mainCountryList.countryList.sort((c1,c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+        return new ResponseEntity<>(CountriesApplication.mainCountryList.countryList,HttpStatus.OK);
+    }
 }
